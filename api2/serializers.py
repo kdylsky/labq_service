cb6d7e9846dd639c5fc05f8c19cf62211b633288
apple <apple@apples-MacBook-Pro.local>
2022-11-09 19:44:25 +0900

    [FEAT] 강우량 관련 DB업데이트

--- api2/serializers.py
+++ api2/serializers.py
@@ -21,3 +21,24 @@ class DetailDrainPipeSchema(serializers.ModelSerializer):
     class Meta:
         model = DetailDrainPipe
         fields = "GUBN", "IDN", "MEA_YMD", "MEA_WAL", "SIG_STA", "REMARK"
+
+
+class RainFallSchema(serializers.ModelSerializer):
+    GU_CODE = serializers.IntegerField(source="gu_code")
+    GU_NAME = serializers.CharField(max_length=10, source="gu_name")
+
+    class Meta:
+        model = RainFall
+        fields = "GU_CODE", "GU_NAME"
+
+
+class DetailRainFallSechema(serializers.ModelSerializer):
+    GU_CODE = serializers.IntegerField(source="rainfall.gu_code")
+    RAINGAUGE_CODE = serializers.IntegerField(source="raingauge_code")
+    RAINGAUGE_NAME = serializers.CharField(max_length=10, source="raingauge_name")
+    RAINFALL10 = serializers.IntegerField(source="rainfall10")
+    RECEIVE_TIME =serializers.CharField(max_length=30, source="receive_time")
+
+    class Meta:
+        model = DetailRainFall
+        fields = "GU_CODE", "RAINGAUGE_CODE", "RAINGAUGE_NAME", "RAINFALL10", "RECEIVE_TIME"
--- api2/utils/utils.py
+++ api2/utils/utils.py
@@ -1,7 +1,7 @@
 from api.utils.utils import UrlSetter
 from api.utils.data_enums import DataTpye
-from api2.serializers import DrainPipeSchema, DetailDrainPipeSchema
-from api2.models import DrainPipe, DetailDrainPipe
+from api2.serializers import DrainPipeSchema, DetailDrainPipeSchema, RainFallSchema, DetailRainFallSechema
+from api2.models import DrainPipe, DetailDrainPipe, RainFall, DetailRainFall
 
 data = DataTpye()
 
@@ -35,7 +35,6 @@ class UpdateData:
         obj.save()
     
     def _detaildrainpipe_partial_update(self, data):
-        print(data)
         for i in data:
             obj, flag = DetailDrainPipe.objects.update_or_create(
                 gubn=DrainPipe.objects.get(gubn=int(i["GUBN"])),
@@ -47,17 +46,48 @@ class UpdateData:
                     "sig_sta":i["SIG_STA"]
                 } 
             )
-            print(obj)
-            print(flag)
             if not flag:
                 obj.mea_ymd = i["MEA_YMD"]
                 obj.mea_wal = i["MEA_WAL"]
                 obj.sig_sta = i["SIG_STA"]
             obj.save()
 
+
+
     def update_rainfall(self):
         rain_data = self._get_rainall()
+        serialize_one = RainFallSchema(data=rain_data,many=True, partial=True)
+        serialize_one.is_valid(raise_exception=True)
+        self._rainfall_partial_update(serialize_one.data)
+        
+        serialize_two = DetailRainFallSechema(data=rain_data, many=True,partial=True)
+        serialize_two.is_valid(raise_exception=True)    
+        self._detailrainfall_partial_update(rain_data)
+    
+    def _rainfall_partial_update(self, data):
+        obj, flag = RainFall.objects.get_or_create(
+            gu_code=data[0]["GU_CODE"],
+            defaults={
+                "gu_name": data[0]["GU_NAME"]
+            }
+        )
+        obj.save()
 
+    def _detailrainfall_partial_update(self, data):
+        for i in data:
+            obj, flag = DetailRainFall.objects.update_or_create(
+                gu_code = RainFall.objects.get(gu_code=int(i["GU_CODE"])),
+                raingauge_code = i["RAINGAUGE_CODE"],
+                raingauge_name = i["RAINGAUGE_NAME"],
+                defaults={
+                    "rainfall10":i["RAINFALL10"],
+                    "receive_time":i["RECEIVE_TIME"]
+                } 
+            )
+            if not flag:
+                obj.rainfall10 = i["RAINFALL10"]
+                obj.receive_time = i["RECEIVE_TIME"]
+            obj.save()
 
 class Real_call:
     def call_test(self):
@@ -65,3 +95,4 @@ class Real_call:
         for i in ["01", "02"]:
             a = UpdateData(i)
             a.update_drainpipe()
+            a.update_rainfall()
